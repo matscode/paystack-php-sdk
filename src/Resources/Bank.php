9fa3ab9de9915d9dd9d5899bb431d7b8ce3c40f0
Michael Akanji <matscode@gmail.com>
2022-05-17 21:22:15 +0100

    update library

---
+++ src/Resources/Bank.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Matscode\Paystack\Resources;
+
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\ClientException;
+use Matscode\Paystack\Exceptions\JsonException;
+use Matscode\Paystack\Interfaces\ResourceInterface;
+use Matscode\Paystack\Traits\ResourcePath;
+use Matscode\Paystack\Utility\Helpers;
+use Matscode\Paystack\Utility\HTTP\HTTPClient;
+use stdClass;
+
+class Bank implements ResourceInterface
+{
+    use ResourcePath;
+
+    protected $httpClient;
+
+    /**
+     * @throws \Exception
+     */
+    public function __construct(HTTPClient $HTTPClient)
+    {
+        $this->setBasePath('bank');
+
+        $this->httpClient = $HTTPClient;
+    }
+
+    /**
+     * List available bank information
+     *
+     * @link https://paystack.com/docs/api/#miscellaneous-bank
+     *
+     * @throws GuzzleException
+     * @throws JsonException
+     */
+    public function list(): stdClass
+    {
+        return Helpers::JSONStringToObj($this->httpClient->get($this->makePath())->getBody());
+    }
+
+    /**
+     * @param $bank_code
+     * @param $account_number
+     * @return stdClass
+     * @throws GuzzleException
+     * @throws JsonException
+     */
+    public function resolve($bank_code, $account_number): StdClass
+    {
+        return Helpers::JSONStringToObj($this->httpClient->get($this->makePath('/resolve'), [
+            'query' => [
+                'bank_code' => $bank_code,
+                'account_number' => $account_number,
+            ]
+        ])->getBody());
+    }
+}
